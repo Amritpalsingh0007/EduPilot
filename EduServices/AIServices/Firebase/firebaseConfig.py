008315995e3ab0b49addde591ed0708c5d1cd5fb
Amritpalsingh0007 <amritpalsingh220923@gmail.com>
2025-04-28 21:21:38 +0530

    firebase credentials added

--- EduServices/AIServices/Firebase/firebaseConfig.py
+++ EduServices/AIServices/Firebase/firebaseConfig.py
@@ -1,6 +1,17 @@
+from dotenv import load_dotenv
+import os
+import json
 from firebase_admin import credentials, initialize_app
 
-# Load service account JSON key
-cred = credentials.Certificate("Firebase/serviceAccountKey.json")
+load_dotenv()
 
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+if not firebase_credentials:
+    raise ValueError("FIREBASE_CREDENTIALS environment variable not set")
+print("Firebase credentials loaded successfully.", firebase_credentials)
+# Replace the escape sequence '\\n' with actual newlines
+firebase_credentials = firebase_credentials.replace('\\\\n', '\\n')
+
+# Now load the credentials
+cred = credentials.Certificate(json.loads(firebase_credentials))
 initialize_app(cred)
