dff6fba4c21af9415efc369cc1cf12fe692a4b79
Amritpalsingh0007 <amritpalsingh220923@gmail.com>
2025-05-01 08:55:34 +0530

    allowing access to vercel deployment

--- EduServices/AIServices/main.py
+++ EduServices/AIServices/main.py
@@ -12,7 +12,7 @@ app.add_middleware(FirebaseAuthMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://edu-pilot-phi.vercel.app","*"],  
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
